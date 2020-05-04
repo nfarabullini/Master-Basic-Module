f4a3e1b0a560917b93ad35cf7d39de1960a3a8cc
nfarabullini <41536517+nfarabullini@users.noreply.github.com>
2020-05-04 17:33:03 +0200

    Update RCAS_Alg.C

--- RCAS_Alg.C
+++ RCAS_Alg.C
@@ -83,7 +83,7 @@ size_t dsc_inc(std::vector<BinaryKey>& keys, Dimension d, size_t g) {
 
     BinaryKey& first_key = keys[0];
 
-    while(g <= first_key.Get(d).size()) {
+    while(g < first_key.Get(d).size()) {
       for(BinaryKey& second_key : keys) {
 	if(first_key.Get(d)[g] != second_key.Get(d)[g]) {
 	  return g;
@@ -126,14 +126,13 @@ struct Node {
   Node *left, *right;
 };
 
-void PrintTree(Node *root, string space) 
-{
+void PrintTree(Node *root, string space) {
   if (root != NULL) { 
     cout << space << endl;
    
     cout << space << "value[";
     if (root -> s_V.size() != 0) {
-      for (int i = 0; i <= root -> s_V.size(); i++) {
+      for (int i = 0; i < root -> s_V.size(); i++) {
 	cout << root -> s_V[i];
       }
     }
@@ -141,7 +140,7 @@ void PrintTree(Node *root, string space)
     
     cout << space << "path[";
     if (root -> s_P.size() != 0) {
-      for (int i = 0; i <= root -> s_P.size(); i++) {
+      for (int i = 0; i < root -> s_P.size(); i++) {
 	cout << root -> s_P[i];
       }
     }
@@ -168,7 +167,7 @@ Node* ConstructRCAS(std::vector<BinaryKey>& keys, Dimension d, size_t g_P, size_
   
     BinaryKey& key_i = keys[0];
     
-     // s_P and s_V should contain all of the bits between g_P and g_Pp (e.g. loop over the key)
+     // s_P and s_V contain all of the bits between g_P and g_Pp (e.g. loop over the key)
     for (int i = g_P; i < g_Pp && i < key_i.Get(Path).size(); i++) {
 	n -> s_P.push_back(key_i.Get(Path)[i]);
     }
@@ -214,84 +213,97 @@ Node* ConstructRCAS(std::vector<BinaryKey>& keys, Dimension d, size_t g_P, size_
 }
 
 void UpdateBuffers(Node *n, std::vector<bool>& buff_V, std::vector<bool>& buff_P) {
-  buff_P = n -> s_P;
-  buff_V = n -> s_V;
+  buff_P.insert(buff_P.end(), n->s_P.begin(), n->s_P.end());
+  buff_V.insert(buff_V.end(), n->s_V.begin(), n->s_V.end());
 }
 
 Matches MatchValue(std::vector<bool>& buff_V, RangeValues& range, Node *n) {
   //lo, hi = longest common prefix between bff_V and (v_l and v_h)
-  // lo <- discriminative bit between buff_V and v_l (similar approach to dsc_inc)
+  //lo <- discriminative bit between buff_V and v_l (similar approach to dsc_inc)
 
   size_t lo = 0;
   size_t hi = 0;
 
-  while (lo < buff_V.size() && buff_V[lo] != range.v_l[lo]) {
+  while (lo < buff_V.size() && lo < range.v_l.size() && buff_V[lo] == range.v_l[lo]) {
     lo++;
   }
 
-  while (hi < buff_V.size() && buff_V[hi] != range.v_h[hi]) {
+  while (hi < buff_V.size() && hi < range.v_h.size() && buff_V[hi] == range.v_h[hi]) {
     hi++;
   }
-
-  string m_V;
    
   if (lo < buff_V.size() && lo < range.v_l.size() && buff_V[lo] < range.v_l[lo]) {
     return MISMATCH;
-  } else if ((hi < buff_V.size() && hi < range.v_l.size()) && buff_V[hi] > range.v_h[hi]) {
+  } else if ((hi < buff_V.size() && hi < range.v_h.size()) && buff_V[hi] > range.v_h[hi]) {
     return MISMATCH;
   }
-  
-  string outcome = "outside";
-  int i = 0;
-    if ((lo < buff_V.size() && lo < range.v_l.size() && buff_V[lo] >= range.v_l[lo]) && (buff_V[hi] <= range.v_h[hi])) {
-      outcome = "inside";
-      i = i;
-    }
-   
-  if (n -> d == Leaf && outcome == "inside") {
-    //buff_V has to present bit 1 sooner than v_l and later than v_h or be identical to one of them: v_l <= buff_V <= v_h
+
+  if ((lo < buff_V.size() && lo < range.v_l.size()) && buff_V[lo] > range.v_l[lo] &&
+      (hi < buff_V.size() && hi < range.v_h.size() && buff_V[hi] < range.v_h[hi])) {
     return MATCH;
-  } else if (n -> d != Leaf && ((lo < buff_V.size() && lo < range.v_l.size()) && buff_V[lo] > range.v_l[lo]) && (buff_V[hi] < range.v_h[hi])) {
+  } else if (n -> d == Leaf) {
     return MATCH;
   } else {
     return INCOMPLETE;
   }
 }
 
-Matches MatchPath(std::vector<bool>& buff_P, std::vector<bool> q, Node *n) {
+Matches MatchPath(std::vector<bool>& buff_P, std::vector<bool> q, Node *n, bool& query_descendant) {
 
   string m_P;
   int i = 0;
+
   while (i < buff_P.size() && i < q.size()) {
-   if(q[i] != buff_P[i]) {
-     return MISMATCH;
+    if(q[i] != buff_P[i]) {
+      return MISMATCH;
     }
     i++;
   }
-
-  if (n -> d == Leaf) {
-    return MATCH;
-  } else {
-    return INCOMPLETE;
-  }
+ // if query_descendant equals true, evaluate everything up until the //, if they match then return MATCH else the code as is
+   switch (query_descendant) { 
+       case true:
+	 while (i < (q.size()-2)) {
+	   if (q[i] == buff_P[i]) {
+	     return MATCH;
+	   }
+	   i++;
+	 }
+       case false:
+	 if (n -> d == Leaf) {
+	   return MATCH;
+	 } else {
+	   return INCOMPLETE;
+	 }
+   }
 }
 
-void CasQuery(Node *n, std::vector<bool> q, RangeValues& range, std::vector<bool>& buff_V, std::vector<bool>& buff_P) {
-  string m_V;
-  string m_P;
-  m_V = MatchValue(buff_V, range, n);
-  m_P = MatchPath(buff_P, q, n);
+void Collect(Node *n) {
   std::vector<string> ref_match;
+  ref_match = n -> reference;
+  for (int i = 0; i < ref_match.size(); i++) {
+    cout << ref_match[i];
+  }
+  cout << "" << endl;
+}
 
-  if (m_V == "MATCH" && m_P == "MATCH") {
-    ref_match = n -> reference;
-    for (int i = 0; i < ref_match.size(); i++) {
-      cout << ref_match[i];
+void CasQuery(Node *n, std::vector<bool>& q, RangeValues& range, std::vector<bool> buff_V, std::vector<bool> buff_P, bool& query_descendant) {
+  Matches m_V;
+  Matches m_P;
+  UpdateBuffers(n, buff_V, buff_P);
+  m_V = MatchValue(buff_V, range, n);
+  m_P = MatchPath(buff_P, q, n, query_descendant);
+
+  if (m_V == MATCH && m_P == MATCH) {
+    if (n -> d == Leaf) {
+      Collect(n);
+    } else {
+      CasQuery(n -> right, q, range, buff_V, buff_P, query_descendant);
+      CasQuery(n -> left, q, range, buff_V, buff_P, query_descendant);
     }
-    cout << "" << endl;
-  } else if (m_V != "MISMATCH" && m_P != "MISMATCH") {
-    CasQuery(n -> right, q, range, buff_V, buff_P);
-    CasQuery(n -> left, q, range, buff_V, buff_P);
+
+  } else if (m_V != MISMATCH && m_P != MISMATCH) {
+      CasQuery(n -> right, q, range, buff_V, buff_P, query_descendant);
+      CasQuery(n -> left, q, range, buff_V, buff_P, query_descendant);
   }
 }
 
@@ -335,12 +347,18 @@ int main()
   size_t g_V = 0;   
 
    ConstructRCAS(keys, Path, g_P, g_V);
-   // set root of node for printing equal to function above
    Node* root = ConstructRCAS(keys, Path, g_P, g_V);
    PrintTree(root, " ");
 
    // path predicate
+   // loop over path predicate and see if at the end there is "//", if yes return query_descendant = TRUE else FALSE
    string query_path = "/bom/item/car/engine$";
+   bool query_descendant = false;
+   for (int i = 0; i < query_path.size(); i++) {
+     if (query_path[i - 1] == '/' && query_path[i] == '/') {
+       query_descendant = true; 
+     }
+   }
    std::vector<bool> q = path_to_binary(query_path);
    
    // value predicate
@@ -349,9 +367,9 @@ int main()
      0X010067E0
    };
    
-     RangeValues range;
-     range.v_l = value_to_binary(v_32[0]);
-     range.v_h = value_to_binary(v_32[1]);
+   RangeValues range;
+   range.v_l = value_to_binary(v_32[0]);
+   range.v_h = value_to_binary(v_32[1]);
 
    std::vector<bool> buff_P;
    std::vector<bool> buff_V;
